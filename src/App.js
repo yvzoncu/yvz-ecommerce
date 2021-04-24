b20c7d042157ffb110f5a2b701ac9bbd29fbe9f5
yavuzoncu <yavuzoncu@protonmail.com>
2021-04-25 01:30:08 +0200

    header added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import HomePage from './pages/HomePage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop-page/shop.component';
+import HeaderComponent from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className="title">{title}</h1>
     <div className="preview">
       {items
-        .filter((item, index) => index < 4)
+        .filter((_, index) => index < 4)
         .map(({ id, ...otherCollectionProps }) => (
           <CollectionItem key={id} {...otherCollectionProps} />
         ))}
